e2441605da3bf7caaf2412a4dd3285bc7ff1e30b
Anders Kusk <anderskusk@gmail.com>
2015-03-12 21:39:27 +0100

    Updated deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,6 +1,6 @@
 # Deploy script
 echo "Installing applications"
-sudo apt-get -y -q -q install xclip nmap hsetroot tmux zsh vlc vim xbindkeys x11-xserver-utils build-essential feh htop
+sudo apt-get -y -q -q install nmap hsetroot tmux zsh vlc vim x11-xserver-utils build-essential feh htop mplayer
 sudo apt-get -y -q -q install libx11-dev libxinerama-dev sharutils suckless-tools chromium-browser urxvt filezilla
 
 echo "Fetching Oh-My-Zsh"
@@ -13,24 +13,19 @@ cd ~/
 
 ln -s ~/dotfiles/ssh .ssh
 ln -s ~/dotfiles/vim .vim
-ln -s ~/dotfiles/xbindkeysrc .xbindkeysrc
 ln -s ~/dotfiles/vimrc .vimrc
+ln -s ~/dotfiles/difmplayrc .difmplayrc
 ln -s ~/dotfiles/tmux.conf .tmux.conf
 ln -s ~/dotfiles/zshrc .zshrc
 ln -s ~/dotfiles/Xdefaults .Xdefaults
-ln -s ~/dotfiles/xprofile .xprofile
 cp -s ~/dotfiles/oh-my-zsh/compact.zsh-theme ~/.oh-my-zsh/themes/compact.zsh-theme
 
 echo "Setting shell"
-chsh -s /bin/zsh
-
-echo "Installing systemwide tools"
-sudo ln -s ~/dotfiles/bin/multiscreen /usr/sbin/multiscreen
-sudo ln -s ~/dotifles/bin/singlescreen /usr/sbin/singlescreen
+chsh -s /usr/bin/zsh
 
 echo "Setting up urxvt"
-sudo cp /home/a/dotfiles/urxvt/clipboard /usr/lib/urxvt/perl/clipboard
+sudo cp ~/dotfiles/urxvt/clipboard /usr/lib/urxvt/perl/clipboard
 
-echo "Disable guest user"
-sudo sh -c "echo allow-guest=false >> /etc/lightdm/lightdm.conf.d/50-unity-greeter.conf"
+#echo "Disable guest user"
+#sudo sh -c "echo allow-guest=false >> /etc/lightdm/lightdm.conf.d/50-unity-greeter.conf"
 echo 'Done'
