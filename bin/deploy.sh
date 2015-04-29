ddba13de5967262053ec2759ed52322aac092a23
kusk <anderskusk@gmail.com>
2015-04-29 14:47:02 +0200

    Removed dwm and added xfce4

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,12 +1,7 @@
 # Deploy script
 echo "Installing applications"
-sudo apt-get -y -q -q install git curl dwm build-essential libx11-dev libxinerama-dev sharutils nmap hsetroot tmux zsh vlc vim feh htop mplayer
-sudo apt-get -y -q -q install chromium-browser rxvt-unicode filezilla gimp mc sqlitebrowser wireshark git
-
-
-echo "Gitting dotfiles"
-cd ~/
-git clone https://github.com/kusk/dotfiles.git
+sudo apt-get -y -q -q install git curl nmap tmux zsh vlc vim feh htop mplayer mpsyt
+sudo apt-get -y -q -q install chromium-browser filezilla gimp mc sqlitebrowser wireshark
 
 echo "Creating symlinks"
 
@@ -20,16 +15,21 @@ ln -s ~/dotfiles/zshrc .zshrc
 ln -s ~/dotfiles/gdbinit .gdbinit
 rm -rf ~/.Xdefaults
 ln -s ~/dotfiles/Xdefaults .Xdefaults
+cp ~/dotfiles/config/xfce4/terminal/terminalrc ~/.config/xfce4/terminal/
+cp ~/dotfiles/config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/
 
 echo "Gitting oh-my-zsh and vim plugins"
+mkdir ~/.vim/autoload
+mkdir ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone git://github.com/tpope/vim-sensible.git ~/.vim/bundle
+git clone https://github.com/bling/vim-airline ~/.vim/bundle/vim-airline
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 
 
-echo "Setting up urxvt and oh-my-zsh"
-sudo cp ~/dotfiles/urxvt/clipboard /usr/lib/urxvt/perl/clipboard
+#echo "Setting up urxvt and oh-my-zsh"
+#sudo cp ~/dotfiles/urxvt/clipboard /usr/lib/urxvt/perl/clipboard
 cp -s ~/dotfiles/oh-my-zsh/compact.zsh-theme ~/.oh-my-zsh/themes/compact.zsh-theme
 
 echo "Setting shell"
