1276cb88d8e1154e4840f2071d9cac0bd4083ffa
Anders Kusk <anderskusk@gmail.com>
2016-12-24 08:28:11 +0100

    Create xubuntu.sh

---
+++ bin/xubuntu.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+echo Deploying dotfiles...
+ssh-keygen
+ssh-copy-id a@maep.dk
+echo --------------------------------------
+echo Updating apt-cache
+sudo apt-get-repository ppa:numix/ppa
+sudo apt-get -y -q -q update
+echo Upgrading dist
+sudo apt-get -y -q -q dist-upgrade
+sudo apt-get -y -q -q install rofi arc-theme exuberant-ctags mpv htop zsh feh vlc tmux nmap curl build-essential libx11-dev libxinerama-dev sharutils
+sudo apt-get -y -q -q install chromium-browser cmake filezilla mc vim python3-pip redshift xcompmgr masscan libxft-dev numix-icon-theme vim-youcompleteme
+echo --------------------------------------
+echo Deploying Powerline fonts
+cd /tmp
+git clone https://github.com/powerline/fonts  &> /dev/null
+cd /tmp/fonts
+sh install.sh
+cd ~/
+echo --------------------------------------
+echo Deploying dotfiles
+mkdir ~/.ssh
+mkdir ~/.config/xfce4/terminal
+ln ~s ~/dotfiles/xsession ~/.xsession
+ln ~s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/ssh/config ~/.ssh/config
+ln -s ~/dotfiles/themes ~/.themes
+ln -s ~/dotfiles/quicktile.cfg ~/.config/quicktile.cfg
+ln ~s ~/dotfiles/xsession ~/.xsession
+ln ~s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/vimrc ~/.vimrc
+
+echo --------------------------------------
+echo Deploying Oh-My-Zsh
+
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh &> /dev/null
+git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting &> /dev/null
+ln -s ~/dotfiles/zshrc ~/.zshrc
+
+echo --------------------------------------
+echo Deploying vim
+vam install youcompleteme  &> /dev/null
+
+
+echo --------------------------------------
+echo Deploying SublimeText3
+mkdir ~/apps
+scp maep.dk:~/apps/sublime-text-3.zip ~/apps/  &> /dev/null
+cd ~/apps
+unzip sublime-text-3.zip  &> /dev/null
+rm -rf ~/apps/sublime-text-3.zip
+cd ~/
+
+echo --------------------------------------
+echo Deploying SublimeText3 configs
+scp maep.dk:~/apps/sublime-text-3-config.zip ~/.config/  &> /dev/null
+cd ~/.config
+unzip sublime-text-3-config.zip  &> /dev/null
+rm ~/.config/sublime-text-3-config.zip
+cd ~/
+echo --------------------------------------
+echo Updating various settings
+chsh -s /usr/bin/zsh
+curl https://raw.githubusercontent.com/w0ng/vim-hybrid/master/colors/hybrid.vim > ~/.vim/plugged/vim-colorschemes/colors/hybrid.vim
+vim
