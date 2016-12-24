6a3560e429259c2e9725021dac21475f55c8e41b
Anders Kusk <anderskusk@gmail.com>
2016-12-24 08:49:07 +0100

    Update xubuntu.sh

--- bin/xubuntu.sh
+++ bin/xubuntu.sh
@@ -9,7 +9,10 @@ sudo add-apt-repository -y -y ppa:numix/ppa
 sudo apt-get -y -qq update
 echo Upgrading dist
 sudo apt-get -y -qq dist-upgrade
-sudo apt-get -y -qq install rofi arc-theme exuberant-ctags mpv htop zsh feh vlc tmux nmap curl build-essential libx11-dev libxinerama-dev sharutils chromium-browser cmake filezilla mc vim python3-pip redshift xcompmgr masscan libxft-dev numix-icon-theme vim-youcompleteme &> /dev/null
+sudo apt-get -y -qq install git rofi arc-theme exuberant-ctags mpv htop zsh feh vlc tmux nmap curl build-essential libx11-dev libxinerama-dev sharutils chromium-browser cmake filezilla mc vim python3-pip redshift xcompmgr masscan libxft-dev numix-icon-theme vim-youcompleteme &> /dev/null
+echo --------------------------------------
+echo Deploying dotfiles
+git clone http://github.com/kusk/dotfiles ~/.dotfiles
 echo --------------------------------------
 echo Deploying Powerline fonts
 cd /tmp
@@ -20,22 +23,22 @@ cd ~/
 echo --------------------------------------
 echo Deploying dotfiles
 mkdir ~/.config/xfce4/terminal
-ln ~s ~/dotfiles/xsession ~/.xsession
-ln ~s ~/dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/vimrc ~/.vimrc
-ln -s ~/dotfiles/ssh/config ~/.ssh/config
-ln -s ~/dotfiles/themes ~/.themes
-ln -s ~/dotfiles/quicktile.cfg ~/.config/quicktile.cfg
-ln ~s ~/dotfiles/xsession ~/.xsession
-ln ~s ~/dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/vimrc ~/.vimrc
+ln ~s ~/.dotfiles/xsession ~/.xsession
+ln ~s ~/.dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/vimrc ~/.vimrc
+ln -s ~/.dotfiles/ssh/config ~/.ssh/config
+ln -s ~/.dotfiles/themes ~/.themes
+ln -s ~/.dotfiles/quicktile.cfg ~/.config/quicktile.cfg
+ln ~s ~/.dotfiles/xsession ~/.xsession
+ln ~s ~/.dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/vimrc ~/.vimrc
 
 echo --------------------------------------
 echo Deploying Oh-My-Zsh
 
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh &> /dev/null
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting &> /dev/null
-ln -s ~/dotfiles/zshrc ~/.zshrc
+ln -s ~/.dotfiles/zshrc ~/.zshrc
 
 echo --------------------------------------
 echo Deploying vim
