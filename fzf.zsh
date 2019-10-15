91e5b24bf49193ceec636f5dd87e7105997a44ee
Anders Kusk <a@0x85.dk>
2019-10-15 08:31:54 +0200

    Create fzf.zsh

---
+++ fzf.zsh
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/a/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/home/a/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/a/.fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/home/a/.fzf/shell/key-bindings.zsh"
